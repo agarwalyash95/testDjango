1ba1e0de94b664a74dcc820d74d739509769d147
agarwalyash95 <yash30076472@gmail.com>
2021-06-30 22:00:40 +0530

    updatefiles

--- travel/views.py
+++ travel/views.py
@@ -1,21 +1,7 @@
 from django.shortcuts import render
 from .models import destination
 def index(request):
-    dest1 = destination()
-    dest1.name = 'Mumbai'
-    dest1.desc = 'Beautifull city with lots of opportunity'
-    dest1.img = 'images/1.jpg'
 
-    dest2 = destination()
-    dest2.name = 'Delhi'
-    dest2.desc = 'Beautifull city with lots of opportunity'
-    dest2.img = 'images/2.jpg'
-
-    dest3 = destination()
-    dest3.name = 'kolkata'
-    dest3.desc = 'Beautifull city with lots of opportunity'
-    dest3.img = 'images/3.jpg'
-
-    dest = [dest1, dest2, dest3]
+    dest = destination.objects.all()
 
     return render(request, 'index.html', { 'dest' : dest})
